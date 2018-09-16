8c54b8c9464ec743c17d57bcf60422a4395d8c2b
Will Sowerbutts <will@sowerbutts.com>
2018-09-17 00:25:27 +0100

    Add random LED toy

---
+++ teensy/toys/random.asm
@@ -0,0 +1,23 @@
+; Draw pseudo-random pattern on 12 user-controlled LEDs
+; source: http://map.grauw.nl/sources/external/z80bits.html
+;   -- "taken from an old ZX Spectrum game and slightly optimised"
+        ld  de, 0x0000
+again:  ld  a,d
+        ld  h,e
+        ld  l,253
+        or  a
+        sbc hl,de
+        sbc a,0
+        sbc hl,de
+        ld  d,0
+        sbc a,d
+        ld  e,a
+        sbc hl,de
+        jr  nc,rand
+        inc hl
+rand:   ex  de, hl
+		ld  c, 0x11
+		out (c), e
+		inc c
+		out (c), d
+		jr again
